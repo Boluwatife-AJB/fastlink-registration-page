e50f88ed0117bf5c6cbe82d41ba4a1f85aa39d97
boluwatife-ajb <boluwatifeajose@gmail.com>
2023-06-08 18:38:36 +0100

    fix:UI errors

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var showPassword = function (event) {
     var eyeIcon = event.target;
-    var passwordInput = eyeIcon.previousElementSibling;
+    var passwordContainer = eyeIcon.parentElement;
+    var passwordInput = passwordContainer.querySelector('input');
     if (passwordInput.type === 'password') {
         passwordInput.type = 'text';
         eyeIcon.classList.replace('ri-eye-off-line', 'ri-eye-line');
@@ -10,7 +11,15 @@ var showPassword = function (event) {
         eyeIcon.classList.replace('ri-eye-line', 'ri-eye-off-line');
     }
 };
-var showPasswordIcons = document.querySelectorAll('.show-password');
-showPasswordIcons.forEach(function (icon) {
-    icon.addEventListener('click', showPassword);
+var passwordContainers = document.querySelectorAll('.password-container');
+passwordContainers.forEach(function (container) {
+    var passwordInput = container.querySelector('input');
+    var showPasswordIcon = container.querySelector('.show-password');
+    passwordInput.addEventListener('focus', function () {
+        container.classList.add('focused');
+    });
+    passwordInput.addEventListener('blur', function () {
+        container.classList.remove('focused');
+    });
+    showPasswordIcon.addEventListener('click', showPassword);
 });
--- script.ts
+++ script.ts
@@ -1,6 +1,9 @@
 const showPassword = (event: MouseEvent) => {
   const eyeIcon = event.target as HTMLElement;
-  const passwordInput = eyeIcon.previousElementSibling as HTMLInputElement;
+  const passwordContainer = eyeIcon.parentElement as HTMLElement;
+  const passwordInput = passwordContainer.querySelector(
+    'input'
+  ) as HTMLInputElement;
 
   if (passwordInput.type === 'password') {
     passwordInput.type = 'text';
@@ -11,7 +14,20 @@ const showPassword = (event: MouseEvent) => {
   }
 };
 
-const showPasswordIcons = document.querySelectorAll('.show-password');
-showPasswordIcons.forEach((icon) => {
-  icon.addEventListener('click', showPassword);
+const passwordContainers = document.querySelectorAll('.password-container');
+passwordContainers.forEach((container) => {
+  const passwordInput = container.querySelector('input') as HTMLInputElement;
+  const showPasswordIcon = container.querySelector(
+    '.show-password'
+  ) as HTMLElement;
+
+  passwordInput.addEventListener('focus', () => {
+    container.classList.add('focused');
+  });
+
+  passwordInput.addEventListener('blur', () => {
+    container.classList.remove('focused');
+  });
+
+  showPasswordIcon.addEventListener('click', showPassword);
 });
